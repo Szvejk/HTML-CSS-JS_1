084332bb0c603ae452f9f0570d8b1bd7c68fc6d3
Sara Szwejkowska <saraszw@op.pl>
2022-05-27 18:44:05 +0200

    create app.js

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
 const PHOTO = document.querySelectorAll(".photo img");
-PHOTO.forEach((photo) => {
-  photo.addEventListener( "click", () => console.log(photo));
+const POPUP = document.querySelector(".popup");
+const POPUP_CLOSE = document.querySelector("popup__close");
+const POPUP_IMG = document.querySelector(".popup_img");
+PHOTO.forEach ((photo) => {
+  photo.addEventListener("click", (e) => {
+    POPUP.classList.remove("hidden");
+    console.log(e);
+  });
 });
+
+POPUP__CLOSE.addEventListener("click", () => {
+  POPUP.classList.add("hidden");
+});
+const myFunction = () => {};
