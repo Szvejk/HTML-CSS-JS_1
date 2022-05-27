1159f8fb501d92aef12a9834b1be5ef07af633bb
Sara Szwejkowska <saraszw@op.pl>
2022-05-27 19:42:08 +0200

    arrows

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
+const PHOTO = document.querySelectorAll('.photo img');
+const POPUP = document.querySelector('.popup');
+const POPUP_CLOSE = document.querySelector('.popup__close');
+const POPUP_IMG = document.querySelector('.popup__img');
+const ARROW_LEFT = document.querySelector('.popup__arrow--left');
+const ARROW_RIGHT = document.querySelector('.popup__arrow--right');
 
-const PHOTO = document.querySelectorAll(".photo img");
-const POPUP = document.querySelector(".popup");
-const POPUP_CLOSE = document.querySelector(".popup__close");
-// const POPUP_IMG = document.querySelector(".popup__img");
+let currentImgIndex;
 
-PHOTO.forEach((photo) => {
-  photo.addEventListener("click", () => {
-      POPUP.classList.remove("hidden");
-      // console.log(e);
-    });
+PHOTO.forEach((photo, index) => {
+	photo.addEventListener('click', (e) => {
+		POPUP.classList.remove('hidden');
+		POPUP_IMG.src = e.target.src;
+		currentImgIndex = index;
+	});
 });
 
-POPUP_CLOSE.addEventListener("click", () => {
-  POPUP.classList.add("hidden");
+POPUP_CLOSE.addEventListener('click', () => {
+	POPUP.classList.add('hidden');
 });
-// const myFunction = () => {};
+const myFunction = () => {};
+
+ARROW_RIGHT.addEventListener('click', () => {});
